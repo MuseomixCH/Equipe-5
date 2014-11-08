2f799e7c77341f75ccb4e3a3868b5e14f375fb00
david hodgetts <david.demainlalune@gmail.com>
2014-11-08 15:48:07 +0100

    add objects.js and relations.js

---
+++ tissuSubjectif/www/js/objects.js
@@ -0,0 +1,293 @@
+var objects = [
+    {
+        "id": 0,
+        "name": "*",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 0
+    },
+    {
+        "id": 1,
+        "name": "Billet de 20fr dans l'urne rouge",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "Ce billet de 20frs faisait parti d'une somme destinée à acheter un objet qui serait un don de tous les visiteurs ayant franchi ses portes durant l'année 2010",
+        "beaconId": 1
+    },
+	 {
+        "id": 2,
+        "name": "Cadre de tableau",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "",
+        "beaconId": 1
+    }, {
+        "id": 3,
+        "name": "Carnet de croquis oublié",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 4,
+        "name": "Artichaut",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 5,
+        "name": "Chien",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 6,
+        "name": "Chewing gum collé",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 7,
+        "name": "Cravate de surveillant de musée",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 8,
+        "name": "Echelle de l'Escalade",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 9,
+        "name": "Escalier en marbre",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 10,
+        "name": "Extincteur",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "Un extincteur est un appareil de lutte contre l'incendie.",
+        "beaconId": 1
+    }, {
+        "id": 11,
+        "name": "Façade du musée",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 12,
+        "name": "Fantôme",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "Un fantôme est une apparition, une vision ou une illusion interprétée comme la manifestation surnaturelle d'une personne décédée. Les fantômes sont également appelés revenants, spectres ou, plus rarement, ombres.",
+        "beaconId": 1
+    }, {
+        "id": 13,
+        "name": "Fauteuil le plus confortable du musée",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 14,
+        "name": "Instrument de musique",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 15,
+        "name": "Lumière / spot",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 16,
+        "name": "Machine à café",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 17,
+        "name": "Miroir",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 18,
+        "name": "Montagne",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 19,
+        "name": "Motif de marbre",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 20,
+        "name": "Objet à prendre et à remplacer",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 21,
+        "name": "Odeur de bois",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 22,
+        "name": "Parquet qui craque",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 23,
+        "name": "Pause de midi",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 24,
+        "name": "Pièce de monnaie romaine",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 25,
+        "name": "Pierre tombale de la cour de musée",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 26,
+        "name": "Plafond des salles palatines",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 27,
+        "name": "Point de vue vers la ville",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 28,
+        "name": "Porte du châtau de Zizers",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 29,
+        "name": "Radiateur",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 30,
+        "name": "Scarabée égyptien",
+        "image": "30-scarabee.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 31,
+        "name": "Socle d'œuvre de la muse tragique",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 32,
+        "name": "Son - chuchotement",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 33,
+        "name": "Souris ou des miettes",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 34,
+        "name": "Tableau accroché anciennement",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 35,
+        "name": "Tube de raifort",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 36,
+        "name": "Trou de sondage dans le mur",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 37,
+        "name": "Un vide, à remplir ou non",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 38,
+        "name": "Une perspective, point de vue",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    }, {
+        "id": 39,
+        "name": "Univers caché sur le tapis",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 1
+    },
+	{
+        "id": 40,
+        "name": "90 % de la collection",
+        "image": "img.jpg",
+        "sound": "pouet.mp3",
+        "description": "bla",
+        "beaconId": 0
+    },
+];
---
+++ tissuSubjectif/www/js/relations.js
@@ -0,0 +1,253 @@
+var relations = [
+    {
+        "from": 0,
+        "to":1,
+        "name": "?"
+    },
+	    {
+        "from": 0,
+        "to":1,
+        "name": "aie"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "ail"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "Albator"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "aventurier"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "bas"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "beurk"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "brillant"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "caché"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "cadre"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "caillou"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "carré"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "chou"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "chute"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "collant"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "coloré"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "compliqué"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "cool"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "couleur"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "court"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "e=mc2"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "eau"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "éblouissant"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "éléphant"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "endormi"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "ennuyeux"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "étourdi"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "faim"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "fatigue"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "Florence"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "fortune"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "fragmenté"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "froid"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "grand"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "gris"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "haut"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "lumineux"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "mat"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "meuh"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "mikado"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "no way"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "nuage"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "nuit"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "out"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "ouverture"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "paprika"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "petit"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "peur"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "pigment"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "planète"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "prototype"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "quotidien"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "R2D2"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "rapide"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "silencieux"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "swap + swap again"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "transparent"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "triste"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "tyrannosaure"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "vega"
+    },    {
+        "from": 0,
+        "to":1,
+        "name": "wild"
+    },
+	    {
+        "from": 0,
+        "to":1,
+        "name": "loto"
+    },
+]
