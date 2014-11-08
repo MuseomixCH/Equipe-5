ca6a9c12523ffaee82c322fcf88bbd885a676954
david hodgetts <david.demainlalune@gmail.com>
2014-11-08 11:13:18 +0100

    identify beacons

--- db/beacons.js
+++ db/beacons.js
@@ -1,12 +1,52 @@
 var beacons = [
     {
-        "id": 0,
-        "major":42,
-        "minor":42
+        "id": 7,
+        "major":22768,
+        "minor":10473
+    },
+    {
+        "id": 8,
+        "major":21315,
+        "minor":64198
+    },
+    {
+        "id": 4,
+        "major":1307,
+        "minor":50728
+    },
+    {
+        "id": 3,
+        "major":42106,
+        "minor":20222
+    },
+    {
+        "id": 10,
+        "major":10304,
+        "minor":6594
+    },
+    {
+        "id": 5,
+        "major":50444,
+        "minor":14343
+    },
+    {
+        "id": 9,
+        "major":46785,
+        "minor":61725
+    },
+    {
+        "id": 2,
+        "major":9471,
+        "minor":29419
     },
     {
         "id": 1,
-        "major":43,
-        "minor":43
-    }
+        "major":33704,
+        "minor":30477
+    },
+    {
+        "id": 6,
+        "major":33704,
+        "minor":30477
+    },
 ];
