c2189a6d91127e4b6aaf18e3e1c49108a1ace9f0
david hodgetts <david.demainlalune@gmail.com>
2014-11-08 13:34:14 +0100

    add jquery + add beacons list

--- tissuSubjectif/www/js/Tissu.js
+++ tissuSubjectif/www/js/Tissu.js
@@ -4,6 +4,15 @@ var Tissu = (function(){
 
     function onRange(beaconInfo){
         console.log('onRange----------------------------');
+          // Sort beacons by distance.
+        beaconInfo.beacons.sort(function(beacon1, beacon2) {
+            return beacon1.distance > beacon2.distance; });
+
+        // Generate HTML for beacons.
+        $.each(beaconInfo.beacons, function(key, beacon){
+            var element = $(createBeaconHTML(beacon));
+            $('#id-screen-range .style-beacon-list').append(element);
+        });
         //displayBeconInfo(beaconInfo);
     }
 
