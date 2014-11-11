0a59a4854f655283e70d56862df478817855d92f
david hodgetts <david.demainlalune@gmail.com>
2014-11-11 11:24:33 +0100

    add relations_all

---
+++ tissuSubjectif/www/js/relations_all.js
@@ -0,0 +1,327 @@
+var relations = [
+    {
+        "from": 11,
+        "to":4,
+        "name": "?"
+    },
+	{
+        "from": 8,
+        "to":37,
+        "name": "..."
+    },
+	{
+        "from": 27,
+        "to":3,
+        "name": "aie"
+    },
+	{
+        "from": 25,
+        "to":35,
+        "name": "Albator"
+    },
+	{
+        "from": 7,
+        "to":8,
+        "name": "aventurier"
+    },
+	{
+        "from": 9,
+        "to":16,
+        "name": "bas"
+    },
+	{
+        "from": 23,
+        "to":35,
+        "name": "beurk"
+    },
+	{
+        "from": 4,
+        "to":21,
+        "name": "caillou"
+    },
+	{
+        "from": 19,
+        "to":13,
+        "name": "carré"
+    },
+	{
+        "from": 3,
+        "to":5,
+        "name": "chou"
+    },
+	{
+        "from": 33,
+        "to":31,
+        "name": "chute"
+    },
+	{
+        "from": 16,
+        "to":17,
+        "name": "collant"
+    },
+	{
+        "from": 21,
+        "to":32,
+        "name": "coloré"
+    },    
+	{
+        "from": 19,
+        "to":24,
+        "name": "rond"
+    },
+	{
+        "from": 22,
+        "to":28,
+        "name": "compliqué"
+    },    
+	{
+        "from": 28,
+        "to":13,
+        "name": "poétique"
+    },
+	{
+        "from":34,
+        "to":18,
+        "name": "cool"
+    },
+	{
+        "from": 40,
+        "to":29,
+        "name": "couleur"
+    },
+	{
+        "from":14,
+        "to":19,
+        "name": "court"
+    },
+	{
+        "from": 39,
+        "to":37,
+        "name": "e=mc2"
+    },
+	{
+        "from": 10,
+        "to":40,
+        "name": "eau"
+    },
+	{
+        "from": 17,
+        "to":27,
+        "name": "éblouissant"
+    },
+	{
+        "from": 6,
+        "to":18,
+        "name": "éléphant"
+    },
+	{
+        "from": 28,
+        "to":14,
+        "name": "endormi"
+    },
+	{
+        "from": 41,
+        "to":11,
+        "name": "étourdi"
+    },
+	{
+        "from": 5,
+        "to":6,
+        "name": "faim"
+    },
+	{
+        "from": 14,
+        "to":9,
+        "name": "fatigue"
+    },
+    {  
+	   "from": 15,
+        "to":34,
+        "name": "Florence"
+    },
+	{
+        "from": 1,
+        "to":26,
+        "name": "fortune"
+    },
+	{
+        "from": 21,
+        "to":28,
+        "name": "fragmenté"
+    },
+	{
+        "from": 16,
+        "to":24,
+        "name": "froid"
+    },    
+	{
+        "from": 41,
+        "to":4,
+        "name": "bleu"
+    },
+	{
+        "from": 4,
+        "to":28,
+        "name": "grand"
+    },   
+	{
+        "from": 41,
+        "to":22,
+        "name": "nuage"
+    }, 	
+	{
+        "from": 34,
+        "to":33,
+        "name": "gris"
+    },
+	{
+        "from": 13,
+        "to":23,
+        "name": "haut"
+    },
+	{
+        "from": 13,
+        "to":25,
+        "name": "lumineux"
+    },
+	{
+        "from": 32,
+        "to":13,
+        "name": "meuh"
+    },
+	{
+        "from": 8,
+        "to":15,
+        "name": "mikado"
+    },
+	{
+        "from": 35,
+        "to":38,
+        "name": "no way"
+    },
+	{
+        "from": 32,
+        "to":25,
+        "name": "nuit"
+    },
+	{
+        "from": 27,
+        "to":7,
+        "name": "out"
+    },
+	{
+        "from": 14,
+        "to":16,
+        "name": "ouverture"
+    },
+	{
+        "from": 35,
+        "to":8,
+        "name": "petit"
+    },
+	{   
+	"from": 6,
+        "to":12,
+        "name": "peur"
+    },
+	{
+        "from": 11,
+        "to":21,
+        "name": "wild card"
+    },
+	{
+        "from": 38,
+        "to":39,
+        "name": "planète"
+    },
+	{
+        "from": 10,
+        "to":29,
+        "name": "prototype"
+    },
+	{
+        "from": 24,
+        "to":27,
+        "name": "quotidien"
+    },
+	{
+        "from": 33,
+        "to":10,
+        "name": "rapide"
+    },
+	{
+        "from": 22,
+        "to":14,
+        "name": "silencieux"
+    },
+	{
+        "from": 26,
+        "to":38,
+        "name": "Genève"
+    },
+	{
+        "from": 3,
+        "to":34,
+        "name": "pigment"
+    },
+	{
+        "from": 7,
+        "to":3,
+        "name": "transparent"
+    },
+	{
+        "from": 31,
+        "to":29,
+        "name": "glaçon"
+    },   
+	{
+        "from": 26,
+        "to":39,
+        "name": "gouache"
+    },
+	{
+        "from": 34,
+        "to":31,
+        "name": "merlot"
+    }, 
+	{
+        "from": 31,
+        "to":36,
+        "name": "fleur"
+    }, 
+	{
+        "from": 25,
+        "to":1,
+        "name": "clown"
+    }, 
+	{
+        "from": 23,
+        "to":7,
+        "name": "contrôle"
+    }, 
+	{
+        "from": 37,
+        "to":15,
+        "name": "hiver"
+    }, 
+	{
+        "from": 18,
+        "to":36,
+        "name": "beauté"
+    }, 
+	{
+        "from": 24,
+        "to":7,
+        "name": "ami"
+    }, 
+	{
+        "from": 37,
+        "to":33,
+        "name": "tyrannosaure"
+    },
+	{
+        "from": 1,
+        "to":38,
+        "name": "loto"
+    }
+]
